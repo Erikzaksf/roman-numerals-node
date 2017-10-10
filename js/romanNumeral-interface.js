b69e3780f06104f741c457721348d696422ed404
Erik Zakrzewski and Kristen Kulha <kristen@>
2017-10-10 09:25:14 -0700

    Separate font and back end logic with a module and export.

--- js/romanNumeral-interface.js
+++ js/romanNumeral-interface.js
@@ -1,11 +1,12 @@
+var RomanNumeral = require('./../js/romanNumeral.js').romanNumeralModule;
 
 $(document).ready(function() {
   $("form#regularNumber").submit(function(event) {
     event.preventDefault();
-  var numberInput = parseInt($("input#number").val());
-    var result = converter(numberInput);
+    var numberInput = parseInt($("input#number").val());
+    var romanNumeral = new RomanNumeral();
+    var result = romanNumeral.converter(numberInput);
     $(".well").show();
     $(".rNumeral").text(result);
-
   });
 });
--- js/romanNumeral.js
+++ js/romanNumeral.js
@@ -1,20 +1,21 @@
+function RomanNumeral() {}
 
-var converter = function(num) {
+RomanNumeral.prototype.converter = function(num) {
 
-var result = "";
-var numberInput = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-var newNumber = "";
-var romans = ["M", "CM", "D", 'CD', "C", "XC", "L", "XL", "X", "IX",  "V", "IV", "I" ];
+  var result = "";
+  var numberInput = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var newNumber = "";
+  var romans = ["M", "CM", "D", 'CD', "C", "XC", "L", "XL", "X", "IX",  "V", "IV", "I" ];
 
 
-for (var i = 0; i < numberInput.length; i++) {
-    while (numberInput[i] <= num) {
-      result += romans[i];
-      num -= numberInput[i];
-    }
+  for (var i = 0; i < numberInput.length; i++) {
+      while (numberInput[i] <= num) {
+        result += romans[i];
+        num -= numberInput[i];
+      }
 
+  }
+  return result;
 }
-return result;
 
-
-}
+exports.romanNumeralModule = RomanNumeral;
